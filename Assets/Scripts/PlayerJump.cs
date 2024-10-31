10ceb088251f14dba8cde4aa0885197b50857ce1
Marcosky1 <144145890+Marcosky1@users.noreply.github.com>
2024-10-31 15:11:23 -0500

    victorrama

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerJump : MonoBehaviour
 {
     public float speed = 0.5f;
+    public float jumpForce;
     private Vector2 movementInput;
     private Rigidbody2D rigidbody2D;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -16,14 +22,29 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
+    private void CheckIfGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+    }
     void Update()
     {
+        CheckIfGrounded();
         //rigidbody2D.AddForce(new Vector2(movementInput.x * speed , ForceMode2D.Impulse));       
         rigidbody2D.AddForce(movementInput * speed, ForceMode2D.Force);
-
     }
     public void ReadDirection(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
+    }
+    public void readJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        rigidbody2D.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
     }
 }
